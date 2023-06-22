e5ccef677e33bd52c768fce3ed0ccbdd58960c21
skyranger05 <ian.capri@live.com>
2023-06-22 09:59:55 -0700

    Edited the two Python files & added a requirement text.

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -13,6 +13,11 @@ from bip44 import Wallet
 from web3 import Account
 from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3 import Web3
+from web3 import EthereumTesterProvider
+w3 = Web3()
+provider = EthereumTesterProvider
+w3 = Web3(provider)
 
 ################################################################################
 # Wallet functionality
--- fintech_finder.py
+++ fintech_finder.py
@@ -28,8 +28,11 @@
 import streamlit as st
 from dataclasses import dataclass
 from typing import Any, List
-from web3 import Web3
-w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+from web3 import Web3, EthereumTesterProvider
+# w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545')) 
+w3 = Web3()
+provider = EthereumTesterProvider()
+w3 = Web3(provider)
 ################################################################################
 # Step 1:
 # Import Ethereum Transaction Functions into the Fintech Finder Application
@@ -79,7 +82,7 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 # @TODO:
 # From `crypto_wallet.py import the functions generate_account, get_balance,
 #  and send_transaction
-from crypto_wallet.py import generate_account, get_balance
+from crypto_wallet import generate_account, get_balance, send_transaction
 ################################################################################
 # Fintech Finder Candidate Information
 
@@ -145,7 +148,15 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-st.sidebar.write(get_balance())
+# account = w3.eth.accounts
+# st.sidebar.write(balance)
+# my_account = account[2]
+# st.sidebar.write(my_account)
+# balance = get_balance(my_account)
+# st.sidebar.write(balance)
+# balance = get_balance("0xC2a5BF24d600fb83dC1E30734D058083830d725b")
+# st.sidebar.write(balance)
+st.sidebar.markdown("### :red[\*\*I had trouble with HTTP provider, so I switched to EthereumTesterProvider. Still have problem loading the address to show balance.\*\*]")
 
 ##########################################
 
@@ -280,7 +291,7 @@ if st.sidebar.button("Send Transaction"):
 
 # The function that starts the Streamlit application
 # Writes FinTech Finder candidates to the Streamlit page
-get_people()
+get_people(w3)
 
 ################################################################################
 # Step 3: Inspect the Transaction
